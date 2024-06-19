e72aeb038d36c7daaa9bd2d3e62679df85a06530
Laddge <dev.laddge@gmail.com>
2024-06-19 19:38:13 +0900

    feat: :sparkles: 数式を入力して遊べるようになった

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
-  import { Button } from '$lib/components/ui/button'
+  import { onMount } from 'svelte'
+  import { Input } from '$lib/components/ui/input'
+  import { Label } from '$lib/components/ui/label'
   import Katex from 'svelte-katex'
   import 'katex/dist/katex.min.css'
+
+  let value: string = 'y = ax + b'
 </script>
  
-<Katex>\KaTeX</Katex>
+<div class="p-6 max-w-4xl mx-auto grid gap-6">
+  <div>
+    <Label for="input">数式</Label>
+    <Input bind:value id="input" />
+  </div>
+  <Katex displayMode>{value}</Katex>
+</div>
