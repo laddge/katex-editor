c6231144c8173f488f334a86576d857640a05aa1
Laddge <dev.laddge@gmail.com>
2024-06-19 19:53:56 +0900

    feat: :sparkles: タイトル等を整備

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,9 @@
   let value: string = 'y = ax + b'
 </script>
  
+<h1 class="text-2xl font-bold text-center py-3 bg-sky-100">TeX数式ビューワー</h1>
 <div class="p-6 max-w-4xl mx-auto grid gap-6">
+  <p><Katex>\TeX</Katex>の数式を表示します.</p>
   <div>
     <Label for="input">数式</Label>
     <Input bind:value id="input" />
