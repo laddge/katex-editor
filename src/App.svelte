5daf48301e092766038e4b88bf04e4a34d80ef77
Laddge <dev.laddge@gmail.com>
2024-06-19 18:40:43 +0900

    feat: :sparkles: KaTeXの導入

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { Button } from '$lib/components/ui/button'
+  import Katex from 'svelte-katex'
+  import 'katex/dist/katex.min.css'
 </script>
  
-<Button>Click me</Button>
+<Katex>\KaTeX</Katex>
